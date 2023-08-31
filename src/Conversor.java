308b6222c802ae480c5ff627f8f4a06b7a194e45
Catalina Laiton <laitonlaura38@gmail.com>
2023-08-30 19:33:13 -0500

    Se arreglaron los errores dentro de la conversion, aun persiste el error al convertir un numero mayor a 5500 de base 512 a 1024, de resto funciona perfecto

--- src/Conversor.java
+++ src/Conversor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Conversor{
+public class Conversor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -23,39 +23,59 @@ public class Conversor{
             return;
         }
 
-        // Definir los caracteres válidos para bases hasta 512
+        if (tieneRelacionPotencial(baseActual, nuevaBase)) {
+            System.out.println("Las bases tienen una relación de potencia.");
+        } else {
+            System.out.println("Las bases no tienen una relación de potencia.");
+        }
+
         String caracteresBaseActual = "0123456789ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" +
                 "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワ'" +
                 "ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ!" +
                 "¡#%&*∙\"'′″„¨`´,./:;?¿@~-−_¯¦|‹›«∀∂∃∇∈∉$¸»ªâÂäÄÃãåÅæÆÇçÐðèÈéÉêÊëËƒìÌíÍîÎïÏⁿòÒóÓôÔöÖÕõØøŒœßÞþùÙúÚûÛüÜÿ™()[]{}+-×÷^=≠<>±≈≤≥₀¹²³⁴⁵⁶⁷⁸⁹‰¼½¾∞═│║┌╒╓╗┐╕╖╗└╘╙╚┘╛╜╝├╞╟╠┤╡╢╣┬╤╥╦┴╧╨╩┼╪╫╬§©©¬®°µ¶·♠♣♥♦†‡•αβΓγΓδΔεζηΘθικΛλμνξΞπΠρΣσΣσςτυΦφχψΩωℵ⌂⌐⌠⌡◊↑→⇒↓←↔⇔─∏∑√∝∧∨∩∪∫∫∴≡⊂⊃⊆⊇БВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯбвгдежзийклмнопрстуфхцчшщъыьэюяαβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσσςτυΦφΧχΨψΩω©®™€£$¥¢₹₽"+
                 "ハ÷Î¼√©—ôˆ§β«¯Γσ√°₹ãτ¬Â√°Ø†≤Σ−Î°ι¨γ¯λçΦΛγφÂ€âμ■вÎжêζÏÎΣ−βθαªГ¬ΛεâÎГπⁿâμ½Þλφô−В‹ÂÐφ§‚¦òτêΓτυВ¹Θ◊ДÐΔ¦±τθ■π§òÎσгΛ±ÎΘφ¤ëθ§ЙºиâΩ¦φ−„Θ‡½αôфр¦Δ≠£ÑÜä√¸öⁿâ◊âΓΘμ◊ô„¯≠ÏΦπτ†αΦµΣΦΓªгπ°⌠¤ïВ±πθμê¸»ãΓτâ■ηâá»êÎΔηαÏâµεüёθâτæâê•¼¦Î±λ¥■§ΣêÎΦγï±¬¯ï−ΛÎΛ−τÎτ†Ω´Î­•φτβΓΓ¬‰ΩÎΛκΩÎ‰¦γΘΩΣΣòεΩ­τ¯ⁿτ¢η╔«³ΩÎαΦπⁿòΘ§îρεÎΘΛÎ§ΛΛΩτ⌡Θ▒½¤ατλττ⌠ΩÏ■ΓìΛΣÏΛρ≠■ΦØΓ¦ΓυωÎε−τΦ†−ΣΓ†ΛΓ−ΦαΓÎε■π−−©⇔âεα≥¦≡α▒αΓΛΣα§μτωτ";
-
         String caracteresNuevaBase = "0123456789ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" +
                 "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワ'" +
                 "ガギグゲゴザジズゼゾダヂヅデドバビブベボパピプペㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎ!" +
                 "¡#%&*∙\"'′″„¨`´,./:;?¿@~-−_¯¦|‹›«∀∂∃∇∈∉$¸»ªâÂäÄÃãåÅæÆÇçÐðèÈéÉêÊëËƒìÌíÍîÎïÏⁿòÒóÓôÔöÖÕõØøŒœßÞþùÙúÚûÛüÜÿ™()[]{}+-×÷^=≠<>±≈≤≥₀¹²³⁴⁵⁶⁷⁸⁹‰¼½¾∞═│║┌╒╓╗┐╕╖╗└╘╙╚┘╛╜╝├╞╟╠┤╡╢╣┬╤╥╦┴╧╨╩┼╪╫╬§©©¬®°µ¶·♠♣♥♦†‡•αβΓγΓδΔεζηΘθικΛλμνξΞπΠρΣσΣσςτυΦφχψΩωℵ⌂⌐⌠⌡◊↑→⇒↓←↔⇔─∏∑√∝∧∨∩∪∫∫∴≡⊂⊃⊆⊇БВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯбвгдежзийклмнопрстуфхцчшщъыьэюяαβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝνΞξΟοΠπΡρΣσσςτυΦφΧχΨψΩω©®™€£$¥¢₹₽"+
                 "ハ÷Î¼√©—ôˆ§β«¯Γσ√°₹ãτ¬Â√°Ø†≤Σ−Î°ι¨γ¯λçΦΛγφÂ€âμ■вÎжêζÏÎΣ−βθαªГ¬ΛεâÎГπⁿâμ½Þλφô−В‹ÂÐφ§‚¦òτêΓτυВ¹Θ◊ДÐΔ¦±τθ■π§òÎσгΛ±ÎΘφ¤ëθ§ЙºиâΩ¦φ−„Θ‡½αôфр¦Δ≠£ÑÜä√¸öⁿâ◊âΓΘμ◊ô„¯≠ÏΦπτ†αΦµΣΦΓªгπ°⌠¤ïВ±πθμê¸»ãΓτâ■ηâá»êÎΔηαÏâµεüёθâτæâê•¼¦Î±λ¥■§ΣêÎΦγï±¬¯ï−ΛÎΛ−τÎτ†Ω´Î­•φτβΓΓ¬‰ΩÎΛκΩÎ‰¦γΘΩΣΣòεΩ­τ¯ⁿτ¢η╔«³ΩÎαΦπⁿòΘ§îρεÎΘΛÎ§ΛΛΩτ⌡Θ▒½¤ατλττ⌠ΩÏ■ΓìΛΣÏΛρ≠■ΦØΓ¦ΓυωÎε−τΦ†−ΣΓ†ΛΓ−ΦαΓÎε■π−−©⇔âεα≥¦≡α▒αΓΛΣα§μτωτ";
 
-        // Convertir a decimal
-        int decimalValue = customParseInt(numero, caracteresBaseActual, baseActual);
-
-        // Convertir a la nueva base
-        String nuevoNumero = customToString(decimalValue, caracteresNuevaBase, nuevaBase);
-
-        // Convertir ambos números a base 10 para verificar
-        int originalDecimal = customParseInt(numero, caracteresBaseActual, baseActual);
-        int convertedDecimal = customParseInt(nuevoNumero, caracteresNuevaBase, nuevaBase);
-
-        // Comprobación de relación potencial entre bases
-        boolean tienenRelacionPotencial = tieneRelacionPotencial(baseActual, nuevaBase);
-        System.out.println("Relación potencial entre bases: " + (tienenRelacionPotencial ? "Sí" : "No"));
+        String nuevoNumero = convertirBase(numero, caracteresBaseActual, baseActual, caracteresNuevaBase, nuevaBase);
 
         System.out.println("El número " + numero + " en base " + baseActual +
                 " es equivalente a " + nuevoNumero + " en base " + nuevaBase);
 
-        System.out.println("Verificación:");
+        int originalDecimal = customParseInt(numero, caracteresBaseActual, baseActual);
+        int convertedDecimal = customParseInt(nuevoNumero, caracteresNuevaBase, nuevaBase);
+
         System.out.println("Número original en base 10: " + originalDecimal);
         System.out.println("Número convertido en base 10: " + convertedDecimal);
+    }
+
+    private static String convertirBase(String numero, String caracteresBaseActual, int baseActual, String caracteresNuevaBase, int nuevaBase) {
+        StringBuilder sb = new StringBuilder();
+        int numBaseActual = 0;
+        int potencia = 1;
+
+        for (int i = numero.length() - 1; i >= 0; i--) {
+            char digitChar = numero.charAt(i);
+            int digitValue = caracteresBaseActual.indexOf(digitChar);
+
+            if (digitValue == -1 || digitValue >= baseActual) {
+                throw new IllegalArgumentException("Carácter inválido en el número.");
+            }
+
+            numBaseActual += digitValue * potencia;
+            potencia *= baseActual;
+        }
+
+        while (numBaseActual > 0) {
+            int digit = numBaseActual % nuevaBase;
+            sb.insert(0, caracteresNuevaBase.charAt(digit));
+            numBaseActual /= nuevaBase;
+        }
+
+        return sb.toString();
     }
 
     private static int customParseInt(String s, String validChars, int radix) {
@@ -70,7 +90,7 @@ public class Conversor{
             if (digitValue == -1 || digitValue >= radix) {
                 throw new IllegalArgumentException("Carácter inválido en el número.");
             }
-
+            
             result += digitValue * power;
             power *= radix;
         }
@@ -78,26 +98,9 @@ public class Conversor{
         return result;
     }
 
-    private static String customToString(int value, String validChars, int newBase) {
-        int base = validChars.length();
-        StringBuilder sb = new StringBuilder();
-
-        if (value == 0) {
-            sb.append(validChars.charAt(0));
-        } else {
-            while (value > 0) {
-                int digit = value % newBase;
-                sb.insert(0, validChars.charAt(digit));
-                value /= newBase;
-            }
-        }
-
-        return sb.toString();
-    }
-
     private static boolean tieneRelacionPotencial(int base1, int base2) {
         if (base1 == base2) {
-            return true;  // Bases iguales siempre tienen relación potencial
+            return true;
         }
 
         int smallerBase = Math.min(base1, base2);
